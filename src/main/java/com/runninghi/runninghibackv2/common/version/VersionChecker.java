91d573dc606c82214d564778f4937fd377116dbc
UncleSubak <kko6369@naver.com>
2024-11-18 20:27:33 +0900

    [#649][Refactor] version check 상세화 및 예외 버전 추가

---
+++ src/main/java/com/runninghi/runninghibackv2/common/version/VersionChecker.java
@@ -0,0 +1,48 @@
+package com.runninghi.runninghibackv2.common.version;
+
+import com.runninghi.runninghibackv2.common.response.ApiResult;
+import com.runninghi.runninghibackv2.config.VersionCheckerConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class VersionChecker {
+
+    private final VersionCheckerConfig config;
+
+    @GetMapping("/test/app-review")
+    public ResponseEntity<ApiResult<Map<String, Boolean>>> checkVersion(@RequestParam("ver") String version) {
+
+        if (config.getExceptions().contains(version)) {
+            return ResponseEntity.ok(ApiResult.success("테스터 확인 성공", Map.of("isReviewer", true)));
+        }
+
+        String[] versionParts = version.split("\\.");
+        String[] currentVersionParts = config.getCurrent().split("\\.");
+
+        StringBuilder targetVersion = new StringBuilder();
+        StringBuilder targetCurrentVersion = new StringBuilder();
+        int arrayLength = currentVersionParts.length;
+        for (int i = 0; i < arrayLength; i++) {
+            targetVersion.append(versionParts[i]);
+            targetCurrentVersion.append(currentVersionParts[i]);
+        }
+
+        if (Integer.parseInt(targetVersion.toString()) > Integer.parseInt(targetCurrentVersion.toString())) {
+            return ResponseEntity.ok(ApiResult.success("테스터 확인 성공", Map.of("isReviewer", true)));
+        } else {
+            return ResponseEntity.ok(ApiResult.success("테스터가 아닙니다.", Map.of("isReviewer", false)));
+        }
+    }
+
+}
