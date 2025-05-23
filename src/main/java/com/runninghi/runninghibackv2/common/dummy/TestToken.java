ccd982b17931d6358320d03206f24b9ab2186d2b
UncleSubak <kko6369@naver.com>
2025-05-24 00:32:34 +0900

    [BugFix] 테스트 토큰 응답 개선

--- src/main/java/com/runninghi/runninghibackv2/common/dummy/TestToken.java
+++ src/main/java/com/runninghi/runninghibackv2/common/dummy/TestToken.java
@@ -19,9 +19,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -31,28 +28,12 @@ public class TestToken {
     private final TestMemberRepository testMemberRepository;
 
     @PostMapping("/test/token")
-    public ResponseEntity<ApiResult<TestTokenResponse>> getTokens() {
+    public ResponseEntity<ApiResult<TestReviewerResponse>> getTokens() {
         try {
-            String adminName = "관리자 : 테스트 계정 이름";
             String userName = "유저 : 테스트 계정 이름";
 
             GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326); // 4326 is the SRID for WGS84
             Point point = geometryFactory.createPoint(new Coordinate(127.543, 36.9876));
-
-            Member admin = testMemberRepository.findByName(adminName)
-                    .orElseGet(() -> {
-                        Member newAdmin = Member.builder()
-                                .alarmConsent(true)
-                                .kakaoId("12345")
-                                .name(adminName)
-                                .nickname("관리자 : 테스트용 관리자입니다.")
-                                .role(Role.ADMIN)
-                                .runDataVO(new RunDataVO(0.0,0.0,2,1))
-                                .geometry(point)
-                                .build();
-                        testMemberRepository.saveAndFlush(newAdmin);
-                        return newAdmin;
-                    });
 
             Member user = testMemberRepository.findByName(userName)
                     .orElseGet(() -> {
@@ -70,46 +51,22 @@ public class TestToken {
                         return newUser;
                     });
 
-            TestTokenResponse response = createTokens(admin, user);
+            // 단일 유저 토큰만 생성
+            AccessTokenInfo userTokenInfo = AccessTokenInfo.from(user);
+            String userAccessToken = jwtTokenProvider.createAccessToken(userTokenInfo);
 
-            return ResponseEntity.ok(ApiResult.success("test용 유저, 어드민 생성 성공. 토큰 발급 성공", response));
+            RefreshTokenInfo userRefreshTokenInfo = RefreshTokenInfo.from(user);
+            String userRefreshToken = jwtTokenProvider.createRefreshToken(userRefreshTokenInfo);
+
+            user.updateRefreshToken(userRefreshToken);
+            testMemberRepository.saveAndFlush(user);
+
+            TokensAndInfo userTokensAndInfo = TokensAndInfo.from(user, userAccessToken, userRefreshToken);
+            TestReviewerResponse response = TestReviewerResponse.from(true, userTokensAndInfo);
+
+            return ResponseEntity.ok(ApiResult.success("테스터 토큰 발급 완료", response));
         } catch (IncorrectResultSizeDataAccessException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResult.error(HttpStatus.BAD_REQUEST, "이미 테스트 유저가 존재함. 서버 에러."));
-        }
-    }
-
-    private TestTokenResponse createTokens(Member admin, Member user) {
-        List<Member> members = new ArrayList<>();
-
-        members.add(admin);
-        members.add(user);
-
-        AccessTokenInfo adminTokenInfo = AccessTokenInfo.from(admin);
-        AccessTokenInfo userTokenInfo = AccessTokenInfo.from(user);
-
-        String adminAccessToken = jwtTokenProvider.createAccessToken(adminTokenInfo);
-        String userAccessToken = jwtTokenProvider.createAccessToken(userTokenInfo);
-
-        RefreshTokenInfo adminRefreshTokenInfo = RefreshTokenInfo.from(admin);
-        RefreshTokenInfo userRefreshTokenInfo = RefreshTokenInfo.from(user);
-
-        String adminRefreshToken = jwtTokenProvider.createRefreshToken(adminRefreshTokenInfo);
-        String userRefreshToken = jwtTokenProvider.createRefreshToken(userRefreshTokenInfo);
-
-        admin.updateRefreshToken(adminRefreshToken);
-        user.updateRefreshToken(userRefreshToken);
-
-        saveAllAndFlushMembers(members);
-
-        TokensAndInfo adminTokensAndInfo = TokensAndInfo.from(admin, adminAccessToken, adminRefreshToken);
-        TokensAndInfo userTokensAndInfo = TokensAndInfo.from(user, userAccessToken, userRefreshToken);
-
-        return TestTokenResponse.from(adminTokensAndInfo, userTokensAndInfo);
-    }
-
-    private void saveAllAndFlushMembers(List<Member> members) {
-        for (Member member : members) {
-            testMemberRepository.saveAndFlush(member);
         }
     }
 
